29c74c9a1f3b9bd7fdd5d7d88408769ccb9ee7f0
unknown <cris.ferreirak10@gmail.com>
2024-10-27 21:56:54 -0300

    first commit

---
+++ src/main/java/com/example/kafka_schema_registry/controller/EventController.java
@@ -0,0 +1,20 @@
+package com.example.kafka_schema_registry.controller;
+
+import com.example.kafka_schema_registry.Employee;
+import com.example.kafka_schema_registry.producer.KafkaAvroProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+    @Autowired
+    private KafkaAvroProducer producer;
+
+    @PostMapping("/events")
+    public String sendMessage(@RequestBody Employee employee) {
+        producer.send(employee);
+        return "message published !";
+    }
+}
